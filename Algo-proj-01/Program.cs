b4be1039cdf4a9ed27291f8511fbca775e83afec
DSLewis22 <dl01852@georgiasouthern.edu>
2016-02-23 14:02:17 -0500

    Project finished

--- Algo-proj-01/Program.cs
+++ Algo-proj-01/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
+using Algo_proj_01.Properties;
 
 namespace Algo_proj_01
 {
@@ -14,71 +20,62 @@ namespace Algo_proj_01
         static void Main(string[] args)
         {
             // create a static array to test data with..
-            int[] testaData = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+           // int[] testaData = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+            Dictionary<string, string> allData = getAllData();
+            var data = Resources.MaxSubArray_29;
+            var bleh = data.Trim()
+                .Replace("\r\n", "")
+                .Split(',')
+                .Where(f => !string.IsNullOrEmpty(f))
+                .Select(d => Convert.ToInt32(d))
+                .ToArray();
+            var answer = Utility.FindMaximumSubArray(bleh, 0, bleh.Length - 1);
+            Console.Write(answer.ToString());
 
-            var data = FindMaximumSubArray(testaData, 0, testaData.Length - 1);
 
+           
+           
+           
         }
 
-        public static int[] FindMaxCrossingSubArray(int[] A, int low, int mid, int high)
+        public static Dictionary<string, string> getAllData()
         {
-            int left_sum = -100000;
-            int sum = 0;
-            int max_left = mid;
-            int[] values = new int[3]; // this will contain the respective low, high, sum values for each side.
-            for (int i = mid; i > low; i--)
+            Dictionary<string, string> resourceData = new Dictionary<string, string>();
+            ResourceSet resourceSet = Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            foreach (DictionaryEntry entry in resourceSet)
             {
-                sum += A[i];
-                if (sum > left_sum)
-                {
-                    left_sum = sum;
-                    max_left = i;
-                }
+                var resourceKey = entry.Key;
+                var resourceValue = entry.Value;
+                resourceData.Add(resourceKey.ToString(), resourceValue.ToString());
             }
 
-            int right_sum = -1000000;
-            int max_right = (mid + 1);
-            sum = 0;
-            for (int j = mid + 1; j < high; j++)
-            {
-                sum += A[j];
-                if (sum > right_sum)
-                {
-                    right_sum = sum;
-                    max_right = j;
-                }
-            }
-
-            // return values...
-            values[0] = max_left;
-            values[1] = max_right;
-            values[2] = left_sum + right_sum;
-            return values;
+            return resourceData;
         }
+    }
 
-        public static int[] FindMaximumSubArray(int[] A, int low, int high)
+
+  
+    public struct SubArray
+    {
+        public int Low { get; set; }
+        public int High { get; set; }
+        public int  Sum { get; set; } // the sum of Low + High
+
+        public override string ToString()
         {
-            int mid;
-            int[] values = new int[3];
-            int[] maxSubLow, maxSubHigh, maxCross;
-            if (low == high)
-            {
-                values[0] = low;
-                values[1] = high;
-                values[2] = A[low];
-                return values;
-            }
-            mid = (low + high)/2;
-            maxSubLow = FindMaximumSubArray(A,low,mid);
-            maxSubHigh = FindMaximumSubArray(A, mid + 1, high);
-            maxCross = FindMaxCrossingSubArray(A, low, mid, high);
+            StringBuilder sb = new StringBuilder();
 
-            if(maxSubLow[2] >= maxSubHigh[2] && maxSubLow[2] >= maxCross[2])
-                return maxSubLow;
-            if (maxSubHigh[2] >= maxSubLow[2] && maxSubHigh[2] >= maxCross[2])
-                return maxSubHigh;
+            sb.Append("CSCI 5330 Spring 2016\r\n");
+            sb.Append("David Lewis\r\n");
+            sb.Append("900732205\r\n\r\n");
+            sb.Append("Running data for file TextBookData.csv\r\n");
+            sb.Append(string.Format("{0}= {1}\r\n", "Low", Low));
+            sb.Append(string.Format("{0}= {1}\r\n", "High", High));
+            sb.Append(string.Format("{0}= {1}\r\n", "Max", Sum));
 
-            return maxCross;
+            return sb.ToString();
         }
+
+
     }
 }
